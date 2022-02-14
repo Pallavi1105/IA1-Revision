68d2ec5b530f84cea61b0a8153eeda568677d358
Pallavi1105 <94678026+Pallavi1105@users.noreply.github.com>
2022-02-14 06:21:07 +0000

    p2final

---
+++ p2final.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+int input()
+{
+  int x;
+  printf("enter the value");
+  scanf("%d",&x);
+  return x;
+}
+int cmp(int a, int b, int c)
+{
+  if(a>b&&b>c)
+  {
+    return a;
+  }
+  else if(b>c)
+  {
+    return b;
+  }
+  else 
+  {
+    return c;
+  }
+}
+ void output(int a, int b, int c, int largest)
+ {
+   printf("largest of %d,%d,%d is %d",a,b,c,largest);
+ }
+ int main()
+ {
+  int a,b,c,largest;
+  a=input();
+  b=input();
+  c=input();
+  largest=cmp(a,b,c);
+  output(a,b,c,largest);
+  return 0;
+ }
